90585571104870a3d3e1c15a6431889f08a2c086
Nick <nsand3@gmail.com>
2021-09-11 17:47:37 -0700

    completed auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,34 @@
+const express = require("express");
+const router = new express.Router();
+const ExpressError = require("../expressError");
+const User = require("../models/user");
+const { authenticateJWT } = require("../middleware/auth");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    // authenticate user
+    const user = User.authenticate(username, password);
+    // if user is authenticated, create token and return
+    if (user) {
+      const token = jwt.sign({ username }, SECRET_KEY);
+      User.updateLoginTimestamp(username);
+      return res.json({ token });
+    } else {
+      throw new ExpressError("Invalid username or password", 400);
+    }
+  } catch (error) {
+    return next(error);
+  }
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -10,3 +36,28 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password, first_name, last_name, phone } = req.body;
+    // register user in DB
+    const registeredUser = await User.register(
+      username,
+      password,
+      first_name,
+      last_name,
+      phone
+    );
+    if (registeredUser) {
+      const token = jwt.sign({ username }, token);
+      User.updateLoginTimestamp(username);
+      return res.json({ token });
+    } else {
+      throw new ExpressError("Something went wrong", 400);
+    }
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,19 @@
+const express = require("express");
+const router = new express.Router();
+const ExpressError = require("../expressError");
+const User = require("../models/user");
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
-
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-
 
 /** GET /:username/to - get messages to user
  *
@@ -22,7 +25,6 @@
  *
  **/
 
-
 /** GET /:username/from - get messages from user
  *
  * => {messages: [{id,
@@ -32,3 +34,5 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+module.exports = router;
